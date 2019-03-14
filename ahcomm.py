84f2fadccb8a34aa8f907049fa9c5f0dc8f52457
Nolan Gagnon <nolan.gagnon@maine.edu>
2019-03-13 21:47:54 -0400

    version 1 working

--- ahcomm.py
+++ ahcomm.py
@@ -1,47 +1,139 @@
+#**********************************************************************
+#                                                                    **
+# Title: ahcomm.py                                                   **
+#                                                                    **
+# Author: nolanHg                                                    **
+#                                                                    **
+# Purpose: Creates a header comment for any source code file         **
+#                                                                    **
+# Date: 03/13/2019                                                   **
+#                                                                    **
+#**********************************************************************
+
 import sys
 
-fin = sys.argv[1]
-	
-## Extract filename and extension from fin
-title, ext = fin.split(".")
+###########################################
+## Constants
+###########################################
+H_WIDTH = 71 # width of the header
+P_LINE_WIDTH = 50 # purpose line width
+GAP = 10 # space before text of next line of
+	 # purpose string
 
+
+###########################################
+## Variables
+###########################################
+pidx = 0 # index for purpose string
+
+###########################################
+## Ensure program is used properly
+###########################################
+if (len(sys.argv) < 2):
+	print("\nUsage: python3 ahcomm.py <name of file>\n")
+	exit()
+
+
+###########################################
+## Get the file name and extension from the
+## command line
+###########################################
+fname = sys.argv[1]
+title, ext = fname.split(".")
+
+
+###########################################
+## Get the information the user wants to
+## put in the header
+###########################################
 author = input("Enter author of file: ")
 purpose = input("Enter purpose of file: ")
 date = input("Enter the version date of file: ")
 
-f = open(fin, "r")
+
+###########################################
+## Read the code from the file into a
+## variable: it will be rewritten to the 
+## input file once the header is written
+###########################################
+f = open(fname, "r")
 code = f.read()
 f.close()
 
-f = open(fin, "w")
 
+###########################################
+## Reopen the target file for writing
+###########################################
+f = open(fname, "w")
+
+
+###########################################
+## Determine what language the code is in
+## so that the correct commenting symbol
+## can be used
+###########################################
 if (ext == "m"): ## MATLAB file 
-	csymb = "%"
-elif (ext == "c"): ## C file
-	csymb = "//"
-elif (ext == "py"): ## Python file
-	csymb = "##"
 
-tot_length = 71
-f.write(csymb + (tot_length - 1) * '*' + '\n')
-f.write(csymb + (tot_length - 3) * ' ' + "**\n")
+	csymb = "%" # matlab comment symbol
 
-title_str = " Title: " + title + "." + ext
-author_str =  " Author: " + author
+elif (ext == "c" or ext == "cpp" or ext == "java"): ## C, C++, or Java file
 
-str_list = [title_str, author_str]
+	csymb = "//" # c-style comment symbol
 
+elif (ext == "py" or ext == "r"): ## Python or R file
+
+	csymb = "#" # python and R comment symbol
+
+
+###########################################
+## Write header comment to the file
+###########################################
+f.write(csymb + (H_WIDTH - 1) * '*' + '\n') # top line
+f.write(csymb + (H_WIDTH - 3) * ' ' + "**\n") # 'blank' line
+
+title_str = csymb + " Title: " + title + "." + ext # title line
+f.write(title_str + (H_WIDTH - len(title_str) - 2) * ' ' + "**\n")
+f.write(csymb + (H_WIDTH - 3) * ' ' + "**\n")
+
+author_str =  csymb + " Author: " + author # author line
+f.write(author_str + (H_WIDTH - len(author_str) - 2) * ' ' + "**\n")
+f.write(csymb + (H_WIDTH - 3) * ' ' + "**\n")
+
+## Break purpose string into multiple
+## lines if necessary 
 purpose_words = purpose.split(" ")
+temp_str = ""
+purpose_str = csymb + " Purpose: "
+first = True
+incomplete = True
+while (incomplete):
+
+	if (pidx < len(purpose_words) and len(temp_str + purpose_words[pidx]) < P_LINE_WIDTH):
+
+		temp_str += purpose_words[pidx] + " "
+		pidx += 1
+	else:   
+		purpose_str += ("" if first else csymb + GAP * ' ') + temp_str + \
+			       (H_WIDTH - (GAP + 3 + len(temp_str))) * ' ' + \
+			       "**\n"
+		temp_str = "" # empty the temp string
+		if (pidx == len(purpose_words)):
+			incomplete = False
+		first = False
+
+f.write(purpose_str)
+f.write(csymb + (H_WIDTH - 3) * ' ' + "**\n")
+
+date_str = csymb + " Date: " + date # date line
+f.write(date_str + (H_WIDTH - len(date_str) - 2) * ' ' + "**\n")
+f.write(csymb + (H_WIDTH - 3) * ' ' + "**\n")
+
+f.write(csymb + (H_WIDTH - 1) * '*' + '\n')
 
 
-
-date_str = " Date: " + date
-
-for string in str_list:
-	f.write(csymb + string + (tot_length - len(string) - 3) * ' ' + "**\n")
-	f.write(csymb + (tot_length - 3) * ' ' + "**\n")
-
-f.write(csymb + (tot_length - 1) * '*' + '\n')
-
+###########################################
+## Write the source code back into the file
+## and close the file
+###########################################
 f.write(code)
 f.close()
